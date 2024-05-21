005b5addf867aaad46fd3fb93725b2a3c7385813
GundalaNikhil <gundala.nikhil444@gmail.com>
2024-05-21 20:32:12 +0530

    basic pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Homepage from './components/HomePage/Homepage';
-import Products from './components/Products/Products';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/HomePage/Footer";
+import Homepage from "./components/HomePage/Homepage";
+import Products from "./components/Products/Products";
+import Navbar from "./components/HomePage/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
-      {/* <Products/> */}
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/HomePage/Navbar.js
+++ src/components/HomePage/Navbar.js
@@ -1,15 +1,24 @@
-import { AppBar, Button, IconButton, InputBase, Menu, MenuItem, Toolbar, Typography, useMediaQuery } from '@material-ui/core';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import React, { useState } from 'react'
-import { Nav } from 'react-bootstrap';
+import {
+  AppBar,
+  Button,
+  IconButton,
+  InputBase,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import React, { useState } from "react";
+import { Nav } from "react-bootstrap";
 
 const Navbar = () => {
-
   const classes = useStyles();
   const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   const [menuAnchor, setMenuAnchor] = useState(null);
 
@@ -20,7 +29,6 @@ const Navbar = () => {
   const handleMenuClose = () => {
     setMenuAnchor(null);
   };
-
 
   return (
     <div className={classes.root}>
@@ -38,10 +46,18 @@ const Navbar = () => {
           <Typography variant="h6" className={classes.title}>
             Logo
           </Typography>
-          <Nav className={`mr-auto ${menuAnchor ? classes.disabledNavLink : ''}`}>
-            <Nav.Link href="#home" className={classes.navLink}>Home</Nav.Link>
-            <Nav.Link href="#about" className={classes.navLink}>About</Nav.Link>
-            <Nav.Link href="#products" className={classes.navLink}>Products</Nav.Link>
+          <Nav
+            className={`mr-auto ${menuAnchor ? classes.disabledNavLink : ""}`}
+          >
+            <Nav.Link href="/" className={classes.navLink}>
+              Home
+            </Nav.Link>
+            <Nav.Link href="/about" className={classes.navLink}>
+              About
+            </Nav.Link>
+            <Nav.Link href="/products" className={classes.navLink}>
+              Products
+            </Nav.Link>
           </Nav>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
@@ -53,7 +69,7 @@ const Navbar = () => {
                 root: classes.inputRoot,
                 input: classes.inputInput,
               }}
-              inputProps={{ 'aria-label': 'search' }}
+              inputProps={{ "aria-label": "search" }}
             />
           </div>
           <Button color="inherit">Login</Button>
@@ -67,18 +83,24 @@ const Navbar = () => {
         onClose={handleMenuClose}
       >
         <MenuItem onClick={handleMenuClose}>
-          <Nav.Link href="#home" className={classes.navLink}>Home</Nav.Link>
+          <Nav.Link href="#home" className={classes.navLink}>
+            Home
+          </Nav.Link>
         </MenuItem>
         <MenuItem onClick={handleMenuClose}>
-          <Nav.Link href="#about" className={classes.navLink}>About</Nav.Link>
+          <Nav.Link href="#about" className={classes.navLink}>
+            About
+          </Nav.Link>
         </MenuItem>
         <MenuItem onClick={handleMenuClose}>
-          <Nav.Link href="#products" className={classes.navLink}>Products</Nav.Link>
+          <Nav.Link href="#products" className={classes.navLink}>
+            Products
+          </Nav.Link>
         </MenuItem>
       </Menu>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
 
@@ -88,77 +110,76 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
-    textAlign: 'left',
+    textAlign: "left",
   },
   search: {
-    position: 'relative',
-    borderRadius: '100px',
-    backgroundColor: '#e0e0e0',
-    '&:hover': {
-      backgroundColor: '#d0d0d0',
+    position: "relative",
+    borderRadius: "100px",
+    backgroundColor: "#e0e0e0",
+    "&:hover": {
+      backgroundColor: "#d0d0d0",
     },
     marginRight: theme.spacing(2),
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(3),
-      width: 'auto',
+      width: "auto",
     },
   },
   searchIcon: {
     padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: '#000'
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#000",
   },
   inputRoot: {
-    color: '#000',
-    width: '100%',
-    border: '2px solid #ccc',
-    borderRadius: '100px',
+    color: "#000",
+    width: "100%",
+    border: "2px solid #ccc",
+    borderRadius: "100px",
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
-    '&:focus': {
-      outline: 'none',
+    "&:focus": {
+      outline: "none",
     },
   },
   menuButton: {
     marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
     },
   },
   navbar: {
-    backgroundColor: 'black',
+    backgroundColor: "black",
     // display:"flex",
     // position: "fixed"
   },
   navLink: {
     marginRight: theme.spacing(2),
-    color: '#ffffff',
-    textDecoration: 'none',
-    transition: 'color 0.3s ease',
-    '&:hover': {
-      color: '#cccccc',
+    color: "#ffffff",
+    textDecoration: "none",
+    transition: "color 0.3s ease",
+    "&:hover": {
+      color: "#cccccc",
     },
     // [theme.breakpoints.down('sm')]: {
     //   display: 'none',
     // },
   },
   disabledNavLink: {
-    pointerEvents: 'none',
+    pointerEvents: "none",
     opacity: 0.5,
   },
-}))
-
+}));
